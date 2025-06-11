71b6094eefce51b865e4081cfc77b136aa0d393e
tomashonsa <152276276+tomashonsa@users.noreply.github.com>
2025-06-11 22:31:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,76 +1,81 @@
 import network
 import socket
 import time
-import sys
 import dht
 from machine import Pin
 from puda import vlhkost_pudy
-#dht
-sensor=dht.DHT11(Pin(14))
-#Nastavení Wi-Fi 
+
+# DHT11 senzor
+sensor = dht.DHT11(Pin(14))
+
+# Relé na GPIO15
+rele = Pin(15, Pin.OUT)
+
+#minimalní vlhkost půdy
+min_vlhkost = 30
+# Wi-Fi připojení
 ssid = 'bbbb'
 password = '12345678'
-#relé
-rele = Pin(15, Pin.OUT)
-rele.value(1)
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(ssid, password)
 
-#Čekání na připojení
 max_wait = 10
 while max_wait > 0:
     if wlan.isconnected():
         break
-    print('cekam na pripojeni', max_wait)
+    print('Čekám na připojení...', max_wait)
     max_wait -= 1
     time.sleep(1)
 
-#Kontrola připojení
 if not wlan.isconnected():
-    raise RuntimeError('nefunguje/timeout')
+    raise RuntimeError('Wi-Fi nefunguje / timeout')
 else:
     ip = wlan.ifconfig()[0]
     print('Připojeno, IP =', ip)
 
-#Nastavení HTTP serveru
+# HTTP server
 addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
 s = socket.socket()
 s.bind(addr)
 s.listen(1)
-print('Server bezi na', ip, 'portu 80')
-
-def load_html():
-    try:
-        with open('index.html', 'r') as f:
-            return f.read()
-    except Exception as e:
-        print('Chyba pri cteni index.html:', e)
-        return '<h1>Chyba: nelze nacist index.html</h1>'
+print('Server běží na', ip, 'portu 80')
 
 while True:
     cl, peer = s.accept()
     print('Připojení od', peer)
-    cl_file = cl.makefile('rwb', 0)
-    # Přečteme a ignorujeme HTTP hlavičky z requestu
-    while True:
-        line = cl_file.readline()
-        if not line or line == b'\r\n':
-            break
-    #vypocty pro dht, vlhkost pudy a zapnuti rele
+    request = cl.recv(1024).decode()
+
+    # tlačítko na relé na webu
+    if '/pump_on' in request:
+        rele.value(1) 
+        time.sleep(5)
+        rele.value(0)
+        response = 'HTTP/1.1 303 See Other\r\nLocation: /\r\n\r\n'
+        cl.send(response.encode())
+        cl.close()
+        continue
+
+    # Změření hodnot
     sensor.measure()
     temp = sensor.temperature()
     hum = sensor.humidity()
     procento = vlhkost_pudy()
-    time.sleep(5)
-    rele.value(0)
 
-    # Nacteni html a zobrazení hodnot
-    with open("index.html") as f:
-        html = f.read()
-        html = html.replace("{{teplota}}", str('%3.1f °C' %temp))
-        html = html.replace("{{vlhkost}}", str('%3.1f %%' %hum))
+    if procento < min_vlhkost: # automaticke zalevání
+        rele.value(1) 
+        time.sleep(5)
+        rele.value(0) 
+        
+    # Vygenerování HTML
+    try:
+        with open("index.html", "r") as f:
+            html = f.read()
+        html = html.replace("{{teplota}}", str('%3.1f °C' % temp))
+        html = html.replace("{{vlhkost}}", str('%3.1f %%' % hum))
         html = html.replace("{{puda_procenta}}", str(procento) + " %")
+    except:
+        html = "<h1>Chyba při načtení index.html</h1>"
 
     response = 'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n' + html
     cl.send(response.encode())
